fd7e0680a847e6147be970634d4d736427a56bf1
Abhay Murali <abhaymuralim9@gmail.com>
2025-02-05 00:02:49 +0530

    Login and authentication added : ch 15 done

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,14 +4,9 @@ import z from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
-// const FormSchema = z.object({
-//   id: z.string(),
-//   customerId: z.string(),
-//   amount: z.coerce.number(),
-//   status: z.enum(["pending", "paid"]),
-//   date: z.string(),
-// });
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -110,4 +105,23 @@ export async function deleteInvoice(id: string) {
     console.log(error);
   }
   revalidatePath("/dasboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
